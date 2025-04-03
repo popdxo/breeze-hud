e3535b701811d06bac99055a08de555d63230e7e
brick <63986294+popdxo@users.noreply.github.com>
2025-04-03 17:00:24 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -6,6 +6,7 @@ import { breeze, mc } from '../breeze_defs';
 
 breeze.registerHud('Clock', 'Shows the time', {
   format: new BooleanSetting("12 hour format", "Switches the clock to be 24 hours", false),
+  gayText: new BooleanSetting("Cooler Text", "Switches the fancy text", false),
   position: new PositionSetting("Position", "Change where the HUD is", 80, 40),
   
   update: function() {
@@ -24,13 +25,20 @@ breeze.registerHud('Clock', 'Shows the time', {
       const hour12 = hours > 12 ? hours - 12 : hours;
       this.time = `${hour12}:${formattedMinutes}`;
     }
-
+    this.position.setWidth(parseInt(this.time.length) * 13.5 + 20)
   },
 
   background: function() {
     UIRenderer.roundedRectangle(this.position.getX(), this.position.getY(), this.position.getWidth(), this.position.getHeight(), 5, new Color(255, 255, 255, 255)); // White background
   },
   draw: function() {
-    UIRenderer.text(`${this.time}`, this.position.getX() + 5, this.position.getY() + 5, new Color(255, 255, 255, 255), 30);
+    if (this.gayText.getValue() === false) {
+      UIRenderer.text(`${this.time}`, this.position.getX() + 10, this.position.getY() + 5, new Color(255, 255, 255, 255), 30);
+    }
+  },
+  drawFancy: function() {
+    if (this.gayText.getValue() === true) {
+      UIRenderer.text(`${this.time}`, this.position.getX() + 10, this.position.getY() + 5, new Color(255, 255, 255, 255), 30);
+    }
   },
 });
