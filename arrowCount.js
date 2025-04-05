f3f6b81d0be8c7fd430caa12e2eea59df0622462
brick <63986294+popdxo@users.noreply.github.com>
2025-04-05 00:28:34 -0400

    Update arrowCount.js

--- arrowCount.js
+++ arrowCount.js
@@ -6,32 +6,34 @@ author: popd
 import { breeze, mc } from '../breeze_defs'
 
 breeze.registerHud("Arrow count HUD", "Displays the amount of arrows you have left", {
-  
-  position: new PositionSetting("Position", "Change the HUD position", 50, 50),
-  
-  /*update: function() {
-    this.arrowCount = 0
 
-    for (let i = 0; i < 36; i++ ) {
-      const stack = inventory.getStackInSlot(i)
+    position: new PositionSetting("Position", "Change the HUD position", 60, 60),
 
-      if (stack !== null) {
-        if ( stack.getItemId() == 262 ) {
-          this.arrowCount += stack.getAmount()
+    update: function() {
+        var arrows = 0
+        this.arrowCount = 0;
+
+        for (var i = 0; i < 36; i++ ) {
+            var stack = inventory.getStackInSlot(i);
+            if (stack !== null) {
+                if (stack.getItemId() == 262) {
+                    arrows += stack.getAmount();
+                }
+            }
+            this.arrowCount = arrows
+            this.countLength = `${arrows}`.length * 10
         }
-      }
+    },
 
+    background: function() {
+      UIRenderer.roundedRectangle(this.position.getX(), this.position.getY(), this.position.getWidth(), this.position.getHeight(), 5, new Color(255, 255, 255, 255));
+    },
+    draw: function() {
+        UIRenderer.remoteTexture("https://static.wikia.nocookie.net/minecraft/images/4/41/Arrow.png/revision/latest/thumbnail/width/360/height/360?cb=20200118044944",
+            this.position.getX() + 5, this.position.getY() + 5, 50, 50, 100, 1, new Color(255,255,255,255))
+    },
+    drawFancy: function() {
+        UIRenderer.text(`${this.arrowCount}`, this.position.getX() + this.position.getWidth() - this.countLength - 5, this.position.getY() + this.position.getHeight() - 20,
+            new Color(255,255,255,255), 20)
     }
-
-  },*/
-
-  //background: function() {
-  //  UIRenderer.roundedRectangle(this.position.getX(), this.position.getY(), this.position.getWidth(), this.position.getHeight(), 5, new Color(255, 255, 255, 255));
-  //},
-  //draw: function() {
-  //  
-  //},
-  drawFancy: function() {
-    UIRenderer.text(`${this.arrowCount}`, this.position.getX(), this.position.getY(), new Color(255,255,255,255))
-  }
 })
